c49af1cb4798fa8e9557ea583063d5c495b958b9
havuki <94868908+havuki@users.noreply.github.com>
2023-05-24 13:14:00 +0300

    Update automail.py
    .

--- automail.py
+++ automail.py
@@ -17,24 +17,21 @@ def open_file(filepath):
     with open(filepath, 'r', encoding='utf-8') as infile:
         return infile.read().strip()
     
-
 # Add the code for sending email using Mailgun API
 
 def send_email(subject, body, recipient):
-    api_key = "<ADD YOUR MAILGUN API KEY HERE>"
-    domain = "<ADD YOUR MAILGUN DOMAIN NAME HERE>"
+    api_key = "a9122930cddda57479ce622662ab3529-db4df449-aa636ea5"
+    domain = "sandboxf0cd3be736f24ecb83073d3889ed758d.mailgun.org"
     url = f"https://api.mailgun.net/v3/{domain}/messages"
     return requests.post(
         url,
         auth=("api", api_key),
         data={"from": f"AI Newsletter Service <mailgun@{domain}>",
               "to": recipient,
-              "subject": subject,
+              "subject": subject, 
               "text": body})
 
-# Load the summarized news
-news_summary = open_file('newsletter.txt')
-
+  
 # Load the optimized system prompt for writing the email newsletter
 system_prompt = open_file('prompt1.txt')
 
@@ -44,9 +41,9 @@ openai.api_key = open_file('openaiapikey.txt')
 # Generate the email content using OpenAI GPT-3
 response = openai.Completion.create(
     engine='text-davinci-003',
-    prompt=system_prompt + '\n\n' + news_summary,
+    prompt=system_prompt + '\n\n',
     max_tokens=500
-)
+ )
 email_content = response.choices[0].text.strip()
 
 # Print the generated email content
